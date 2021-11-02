a189c816e63a272bd924fd0ef174c612d9af22e7
Marcin <Marcin@DESKTOP-H5IF89K>
2021-11-02 16:26:25 +0100

    Add project files.

---
+++ TrainingGround/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrainingGround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
